86716dd4441d0f8ea412069f58f06fdbc9fe5ef6
Sanjay Birla <birlasanjay555@gmail.com>
2026-03-21 23:00:32 +0530

    created authROutes and authController

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const connectDB = require('./DB/connect');
+const authRoutes = require('./routes/auth.routes.js')
 
 const app=express();
 
 connectDB();
+
+app.use('/api/auth',authRoutes)
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is started on port ${process.env.PORT}`);
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,6 @@
+
+async function registerUser(req,res){
+    const {name,email,password} = req.body;
+
+    
+}
