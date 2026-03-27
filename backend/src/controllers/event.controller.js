497ab8bbcdabcf29ca302b1c8ce37a86a703acfb
Sanjay Birla <birlasanjay555@gmail.com>
2026-03-27 23:34:25 +0530

    createEvent and getEvents methods

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -26,3 +26,21 @@ async function createEvent(req,res){
     }
 }
 
+async function getEvents(req,res){
+    try {
+        const events = await Event.find().populate("organiser","email role");
+        res.json(events);
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+async function getEventById(req,res){
+    try {
+        const event = await Event.findById(req.params.id).populate("organiser","email role");
+        if(!event) return res.status(404).json({message:"Event not found"})
+        res.json(event);
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
