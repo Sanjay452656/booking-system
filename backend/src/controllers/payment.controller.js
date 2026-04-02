2941df82a96261bcb1636c8e1ea36e67cdf00d17
Sanjay Birla <birlasanjay555@gmail.com>
2026-04-02 22:52:03 +0530

    verify payment function

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -1,5 +1,6 @@
-const razorpay = require("../config/razorpay");
 const Booking = require("../models/booking.model");
+const razorpay = require("../config/razorpay.js")
+const crypto = require('crypto');
 
 
 async function createOrder(){
@@ -19,12 +20,12 @@ async function createOrder(){
         const amount = booking.event.price * booking.quantity;
 
         const options = {
-            amount : amount,
+            amount : amount*100,
             currency : "INR",
             receipt : `receipt_${booking._id}`
         }
 
-        const order = await razorpay.orders.create(options);
+      const order = await razorpay.orders.create(options);
 
       res.json({
        orderId: order.id,
@@ -33,5 +34,43 @@ async function createOrder(){
       });
     } catch (error) {
         res.status(500).json({ message: error.message });
+    }
+}
+
+async function verifyPayment(req,res){
+    try {
+        
+        const {razorpay_order_id,razorpay_payment_id,razorpay_signature,bookingId} = req.body;
+
+        const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+        const expectedSignature = crypto
+           .createHmac("sha256",process.env.RAZORPAY_KEY_SECRET)
+           .update(body)
+           .digest("hex")
+        
+        if(expectedSignature !=razorpay_signature){
+            return res.status(400).json({message: "Invalid Payment"});
+        }
+
+        const booking = await Booking.findById(bookingId);
+
+        if(!booking){
+            return res.status(404).json({message:"Booking not found"});
+        }
+
+        if(booking.status == "PENDING"){
+            return res.status(400).json({message:"Already Processed"})
+        }
+
+        booking.status = "CONFIRMED";
+        await booking.save();
+
+        res.json({
+        message: "Payment successful, booking confirmed",
+        booking,
+    });
+    } catch (error) {
+        res.status(500).json({ message: error.message }); 
     }
 }
