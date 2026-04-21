73e75849f7d157d80db99ec3d77ba605e2712cf1
Sanjay Birla <birlasanjay555@gmail.com>
2026-04-21 23:36:23 +0530

    resolve connection error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Login from './pages/Login';
+import Signup from './pages/SignUp';
 
 createRoot(document.getElementById('root')).render(
    <AuthProvider>
       <BrowserRouter>
         <Navbar />
+        <Login/>
+        <Signup/>
       </BrowserRouter>
     </AuthProvider>,
 )
